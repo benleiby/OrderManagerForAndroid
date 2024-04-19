de1c58f781ce503cd1365683bf32f7002e092ee4
benleiby <105562274+benleiby@users.noreply.github.com>
2024-04-18 20:18:38 -0400

    update dataManager

--- app/src/main/java/com/example/ordermanagerforandroid/DataManager.java
+++ app/src/main/java/com/example/ordermanagerforandroid/DataManager.java
@@ -8,10 +8,10 @@ import java.util.ArrayList;
  * This only needs to be called once during first initialization.
  * @author Benjamin Leiby
  */
-final class DataManager {
+public final class DataManager {
 
-    private static Order currentOrder;
-    private static ArrayList<Order> orderList;
+    private  Order currentOrder;
+    private final ArrayList<Order> orderList;
     private static DataManager dataManager;
 
     private DataManager() {
@@ -19,20 +19,20 @@ final class DataManager {
         orderList = new ArrayList<Order>();
     }
 
-    public static DataManager getDataManager() {
+    public synchronized static DataManager getDataManager() {
         if (dataManager == null) dataManager = new DataManager();
         return dataManager;
     }
 
-    public static ArrayList<Order> getOrderList() {
+    public ArrayList<Order> getOrderList() {
         return orderList;
     }
 
-    public static Order getCurrentOrder() {
+    public Order getCurrentOrder() {
         return currentOrder;
     }
 
-    public static void setCurrentOrder(Order order) {
+    public void setCurrentOrder(Order order) {
         currentOrder = order;
     }
 
